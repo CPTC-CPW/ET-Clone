d624c1fdf3aefde71f28e7bcda8bc8e73a01193d
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2023-05-11 11:52:16 -0700

    Added WASD key press detection
    Co-authored-by: Evan Pendergrass <NullPointer1331@users.noreply.github.com>
    Co-authored-by: John Bartholomew <JohnnyBee86@users.noreply.github.com>
    Co-authored-by: Elias McKee <eqbookfan@gmail.com>
    Co-authored-by: enraky@hotmail.com <enraky@hotmail.com>

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ var canvas = document.getElementById("game-canvas");
 var square = canvas.getContext("2d");
 square.fillStyle = "#000000";
 square.fillRect(25, 25, 50, 50);
+document.addEventListener('keydown', moveDetect);
+function moveDetect(event) {
+    var key = event.key;
+    if (key == 'w' || key == 'W') {
+        alert("W was pressed");
+    }
+    else if (key == 'a' || key == 'A') {
+        alert("A was pressed");
+    }
+    else if (key == 's' || key == 'S') {
+        alert("S was pressed");
+    }
+    else if (key == 'd' || key == 'D') {
+        alert("D was pressed");
+    }
+}
--- ts/script.ts
+++ ts/script.ts
@@ -1,4 +1,25 @@
+// W3Schools has very helpful tutorials for this. 
+// https://www.w3schools.com/graphics/canvas_intro.asp 
+// https://www.w3schools.com/graphics/game_intro.asp
 let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("game-canvas");
 let square = canvas.getContext("2d");
 square.fillStyle = "#000000";
 square.fillRect(25, 25, 50, 50);
+document.addEventListener('keydown', moveDetect);
+
+function moveDetect(event) {
+    let key = event.key;
+    // Checking for upper and lowercase in case user has caps lock on
+    if(key == 'w' || key == 'W') {
+        alert("W was pressed");
+    }
+    else if(key == 'a'|| key == 'A') {
+        alert("A was pressed");
+    }
+    else if(key == 's' || key == 'S') {
+        alert("S was pressed");
+    }
+    else if(key == 'd' || key == 'D') {
+        alert("D was pressed");
+    }
+}

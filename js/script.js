275dae3b831c39bc11ac14b79815f1434f6f5a88
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2023-05-11 11:22:16 -0700

    Square character placeholder drawn on screen

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("game-canvas");
+var square = canvas.getContext("2d");
+square.fillStyle = "#000000";
+square.fillRect(25, 25, 50, 50);
---
+++ ts/script.ts
@@ -0,0 +1,4 @@
+let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("game-canvas");
+let square = canvas.getContext("2d");
+square.fillStyle = "#000000";
+square.fillRect(25, 25, 50, 50);
